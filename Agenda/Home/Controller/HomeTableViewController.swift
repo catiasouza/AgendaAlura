8da75530432fe22747a8a7cb589aedff8c9a2740
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-10-24 11:06:55 -0300

    Adicionando alunos na lista

--- Agenda/Home/Controller/HomeTableViewController.swift
+++ Agenda/Home/Controller/HomeTableViewController.swift
@@ -1,24 +1,23 @@
-//
-//  HomeTableViewController.swift
-//  Agenda
-//
-//  Created by Ândriu Coelho on 24/11/17.
-//  Copyright © 2017 Alura. All rights reserved.
-//
 
 import UIKit
+import  CoreData
 
 class HomeTableViewController: UITableViewController, UISearchBarDelegate {
     
     //MARK: - Variáveis
     
     let searchController = UISearchController(searchResultsController: nil)
-    
+    var gerenciadorResultados: NSFetchedResultsController<Aluno>?
+    var contexto:NSManagedObjectContext{
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configuraSearch()
+        self.recuperaAluno()
     }
     
     // MARK: - Métodos
@@ -28,17 +27,47 @@ class HomeTableViewController: UITableViewController, UISearchBarDelegate {
         self.searchController.dimsBackgroundDuringPresentation = false
         self.navigationItem.searchController = searchController
     }
-
+    func recuperaAluno(){
+        let pesquisaAluno: NSFetchRequest <Aluno> = Aluno.fetchRequest()
+        let ordenaNome = NSSortDescriptor(key: "nome", ascending: true)
+        pesquisaAluno.sortDescriptors = [ordenaNome]
+        
+        gerenciadorResultados = NSFetchedResultsController(fetchRequest: pesquisaAluno, managedObjectContext: contexto, sectionNameKeyPath: nil, cacheName: nil)
+        
+        do{
+            try gerenciadorResultados?.performFetch()
+            
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+        
+    }
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        guard let contadorListaDeAlunos = gerenciadorResultados?.fetchedObjects?.count
+            else { return 0 }
+        return contadorListaDeAlunos
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let celula = tableView.dequeueReusableCell(withIdentifier: "celula-aluno", for: indexPath) as! HomeTableViewCell
+        
+        guard let aluno = gerenciadorResultados?.fetchedObjects![indexPath.row]else
+        {return celula
+                
+        }
+        celula.labelNomeDoAluno.text = aluno.nome
+        
+        if let imagenDoAluno = aluno.foto as? UIImage{
+            celula.imageAluno.image = imagenDoAluno
+        }
 
         return celula
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
